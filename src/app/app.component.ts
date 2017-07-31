890cdb286ced94d9909715ed221c6e48884091ff
abugajska <abugajska>
2017-07-31 14:54:06 +0100

    injecting service into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ListService} from './list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
 
+
+export class AppComponent implements OnInit {
+  constructor(private listService: ListService) {}
+  ngOnInit() {
+    this.listService.getData().subscribe((data) => { console.log("data:", data) })
+    
+  }
 }
--- src/app/list.service.ts
+++ src/app/list.service.ts
@@ -1,19 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Response, Headers, RequestOptions } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
-
-
 
 @Injectable()
 
 export class ListService {
-    constructor(private http: Http) {
-        var obj;
-        this.getJSON().subscribe(data => obj=data, error => console.log(error));
+    constructor(private http: HttpClient) {
     }
-    public getJSON(): Observable<any> {
+
+    public getData(): Observable<any> {
         return this.http.get("./list.json")
             .map((res:any) => res.json())
             .catch((error:any) => {console.log('error');
