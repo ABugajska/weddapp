c3eb100beca51d91e20b7598f9d3a54b04373d40
abugajska <abugajska>
2017-07-31 12:07:14 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListService } from './list.service';
+
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(
+      routes
+    )
   ],
-  providers: [],
+  providers: [ ListService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
